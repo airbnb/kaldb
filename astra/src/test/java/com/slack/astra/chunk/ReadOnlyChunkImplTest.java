73de7056072b8ab8784e9ade05b4667e003ee07c
Aubrey <autata@users.noreply.github.com>
2025-02-07 16:21:33 -0600

    If a chunk fails to load when assigned, run the eviction logic (#22)
    Co-authored-by: Nora Howard <nh@baroquebobcat.com>
    
    fix ReadOnlyChunkImplTest.java after replaying chunk evict logic pr

--- astra/src/test/java/com/slack/astra/chunk/ReadOnlyChunkImplTest.java
+++ astra/src/test/java/com/slack/astra/chunk/ReadOnlyChunkImplTest.java
@@ -34,6 +34,8 @@ import com.slack.astra.metadata.core.CuratorBuilder;
 import com.slack.astra.metadata.replica.ReplicaMetadata;
 import com.slack.astra.metadata.replica.ReplicaMetadataStore;
 import com.slack.astra.metadata.schema.ChunkSchema;
+import com.slack.astra.metadata.schema.FieldType;
+import com.slack.astra.metadata.schema.LuceneFieldDef;
 import com.slack.astra.metadata.search.SearchMetadata;
 import com.slack.astra.metadata.search.SearchMetadataStore;
 import com.slack.astra.metadata.snapshot.SnapshotMetadata;
@@ -190,7 +192,7 @@ public class ReadOnlyChunkImplTest {
     // setup Zk, BlobFs so data can be loaded
     initializeMetadataReplica(replicaMetadataStore, replicaId, snapshotId);
     initializeMetadataSnapshot(snapshotMetadataStore, snapshotId, 0);
-    initializeBlobStorageWithIndex(snapshotId);
+    initializeBlobStorageWithIndex(snapshotId, false);
     initializeCacheNodeAssignment(
         cacheNodeAssignmentStore, assignmentId, snapshotId, cacheNodeId, replicaSet, replicaId);
     initializeCacheNode(cacheNodeMetadataStore, cacheNodeId, "some-host.name", 1, replicaSet, true);
@@ -629,7 +631,7 @@ public class ReadOnlyChunkImplTest {
     // setup Zk, BlobFs so data can be loaded
     initializeMetadataReplica(replicaMetadataStore, replicaId, snapshotId);
     initializeMetadataSnapshot(snapshotMetadataStore, snapshotId, 0);
-    initializeBlobStorageWithIndex(snapshotId);
+    initializeBlobStorageWithIndex(snapshotId, false);
     initializeCacheNodeAssignment(
         cacheNodeAssignmentStore, assignmentId, snapshotId, cacheNodeId, replicaSet, replicaId);
     initializeCacheNode(cacheNodeMetadataStore, cacheNodeId, "some-host.name", 1, "rep1", true);
@@ -794,7 +796,7 @@ public class ReadOnlyChunkImplTest {
     // setup Zk, BlobFs so data can be loaded
     initializeMetadataReplica(replicaMetadataStore, replicaId, snapshotId);
     initializeMetadataSnapshot(snapshotMetadataStore, snapshotId, 29);
-    initializeBlobStorageWithIndex(snapshotId);
+    initializeBlobStorageWithIndex(snapshotId, false);
     initializeCacheNodeAssignment(
         cacheNodeAssignmentStore, assignmentId, snapshotId, cacheNodeId, replicaSet, replicaId);
     initializeCacheNode(cacheNodeMetadataStore, cacheNodeId, "some-host.name", 1, "rep1", true);
@@ -886,6 +888,108 @@ public class ReadOnlyChunkImplTest {
     curatorFramework.unwrap().close();
   }
 
+  @Test
+  public void shouldEvictChunkOnAssignmentFailure() throws Exception {
+    AstraConfigs.AstraConfig AstraConfig = makeCacheConfig();
+    AstraConfigs.MetadataStoreConfig metadataStoreConfig =
+        AstraConfigs.MetadataStoreConfig.newBuilder()
+            .setZookeeperConfig(
+                AstraConfigs.ZookeeperConfig.newBuilder()
+                    .setZkConnectString(testingServer.getConnectString())
+                    .setZkPathPrefix("shouldEvictChunkOnAssignmentFailure")
+                    .setZkSessionTimeoutMs(1000)
+                    .setZkConnectionTimeoutMs(1000)
+                    .setSleepBetweenRetriesMs(1000)
+                    .setZkCacheInitTimeoutMs(1000)
+                    .build())
+            .build();
+
+    AsyncCuratorFramework curatorFramework =
+        CuratorBuilder.build(meterRegistry, metadataStoreConfig.getZookeeperConfig());
+    ReplicaMetadataStore replicaMetadataStore =
+        new ReplicaMetadataStore(curatorFramework, etcdClient, metadataStoreConfig, meterRegistry);
+    SnapshotMetadataStore snapshotMetadataStore =
+        new SnapshotMetadataStore(curatorFramework, etcdClient, metadataStoreConfig, meterRegistry);
+    SearchMetadataStore searchMetadataStore =
+        new SearchMetadataStore(curatorFramework, etcdClient, metadataStoreConfig, meterRegistry, true);
+    CacheSlotMetadataStore cacheSlotMetadataStore =
+        new CacheSlotMetadataStore(curatorFramework, etcdClient, metadataStoreConfig, meterRegistry);
+    CacheNodeAssignmentStore cacheNodeAssignmentStore =
+        new CacheNodeAssignmentStore(curatorFramework, etcdClient, metadataStoreConfig, meterRegistry);
+    CacheNodeMetadataStore cacheNodeMetadataStore =
+        new CacheNodeMetadataStore(curatorFramework, etcdClient, metadataStoreConfig, meterRegistry);
+
+    String replicaId = "foo";
+    String snapshotId = "boo";
+    String assignmentId = "dog";
+    String cacheNodeId = "baz";
+    String replicaSet = "cat";
+
+    // setup Zk, BlobFs so data can be loaded
+    initializeMetadataReplica(replicaMetadataStore, replicaId, snapshotId);
+    initializeMetadataSnapshot(snapshotMetadataStore, snapshotId, 0);
+    initializeBlobStorageWithIndex(snapshotId, true);
+    initializeCacheNodeAssignment(
+        cacheNodeAssignmentStore, assignmentId, snapshotId, cacheNodeId, replicaSet, replicaId);
+    initializeCacheNode(cacheNodeMetadataStore, cacheNodeId, "some-host.name", 1, replicaSet, true);
+
+    SearchContext searchContext =
+        SearchContext.fromConfig(AstraConfig.getCacheConfig().getServerConfig());
+    ReadOnlyChunkImpl<LogMessage> readOnlyChunk =
+        new ReadOnlyChunkImpl<>(
+            curatorFramework,
+            meterRegistry,
+            blobStore,
+            searchContext,
+            AstraConfig.getS3Config().getS3Bucket(),
+            AstraConfig.getCacheConfig().getDataDirectory(),
+            AstraConfig.getCacheConfig().getReplicaSet(),
+            cacheSlotMetadataStore,
+            replicaMetadataStore,
+            snapshotMetadataStore,
+            searchMetadataStore,
+            cacheNodeAssignmentStore,
+            cacheNodeAssignmentStore.getSync(cacheNodeId, assignmentId),
+            snapshotMetadataStore.findSync(snapshotId),
+            cacheNodeMetadataStore,
+            AstraConfig.getLuceneConfig());
+
+    // wait for chunk to register
+    await()
+        .until(
+            () ->
+                readOnlyChunk.getChunkMetadataState()
+                    == Metadata.CacheSlotMetadata.CacheSlotState.FREE);
+
+    assignReplicaToChunk(cacheSlotMetadataStore, replicaId, readOnlyChunk);
+
+    // The expected state transitions are:
+    // ASSIGNED -> LOADING (encounters manufactured error) -> EVICT -> EVICTING -> FREE
+    // The final state being FREE.
+    await()
+        .until(
+            () ->
+                readOnlyChunk.getChunkMetadataState()
+                    == Metadata.CacheSlotMetadata.CacheSlotState.FREE);
+
+    // Ensure that the search metadata was not registered
+    assertThat(searchMetadataStore.listSync().size()).isEqualTo(0);
+
+    // Verify eviction metrics were updated. We expect a successful eviction and a failed
+    // assignment.
+    assertThat(meterRegistry.get(CHUNK_EVICTION_TIMER).tag("successful", "true").timer().count())
+        .isEqualTo(1);
+    assertThat(meterRegistry.get(CHUNK_ASSIGNMENT_TIMER).tag("successful", "false").timer().count())
+        .isEqualTo(1);
+
+    // verify that the directory has been cleaned up
+    try (var files = java.nio.file.Files.list(readOnlyChunk.getDataDirectory())) {
+      assertThat(files.findFirst().isPresent()).isFalse();
+    }
+
+    curatorFramework.unwrap().close();
+  }
+
   private void assignReplicaToChunk(
       CacheSlotMetadataStore cacheSlotMetadataStore,
       String replicaId,
@@ -928,7 +1032,7 @@ public class ReadOnlyChunkImplTest {
             false));
   }
 
-  private void initializeBlobStorageWithIndex(String snapshotId) throws Exception {
+  private void initializeBlobStorageWithIndex(String snapshotId, boolean badData) throws Exception {
     LuceneIndexStoreImpl logStore =
         LuceneIndexStoreImpl.makeLogStore(
             Files.newTemporaryFolder(),
@@ -948,6 +1052,15 @@ public class ReadOnlyChunkImplTest {
     // Create schema file to upload
     ChunkSchema chunkSchema =
         new ChunkSchema(snapshotId, logStore.getSchema(), new ConcurrentHashMap<>());
+
+    // Introduce bad data in the schema. Specifically, the key in the map should match the field
+    // name when the data is "good".
+    if (badData) {
+      chunkSchema.fieldDefMap.put(
+          "field_name",
+          new LuceneFieldDef(
+              "field_name_does_not_match", FieldType.INTEGER.name, true, true, true));
+    }
     File schemaFile = new File(dirPath + "/" + SCHEMA_FILE_NAME);
     ChunkSchema.serializeToFile(chunkSchema, schemaFile);
 
