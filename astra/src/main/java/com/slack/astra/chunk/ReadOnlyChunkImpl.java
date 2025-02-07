9e8e3f2fb50623dcaee5eaa32528110dd19039d9
Aubrey Tatarowicz <aubrey.tatarowicz@gmail.com>
2025-02-07 12:47:27 -0600

    add missing import

--- astra/src/main/java/com/slack/astra/chunk/ReadOnlyChunkImpl.java
+++ astra/src/main/java/com/slack/astra/chunk/ReadOnlyChunkImpl.java
@@ -29,6 +29,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Instant;
+import java.util.EnumSet;
 import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
