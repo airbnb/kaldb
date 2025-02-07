b19563daf86f4c8c496f21c928fe9c3313de1635
Zarna Parekh <zarna1parekh@gmail.com>
2025-02-07 15:14:13 -0800

    More test cases

--- astra/src/test/java/com/slack/astra/writer/SpanFormatterTest.java
+++ astra/src/test/java/com/slack/astra/writer/SpanFormatterTest.java
@@ -2,8 +2,11 @@ package com.slack.astra.writer;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import com.google.protobuf.ByteString;
 import com.slack.astra.proto.schema.Schema;
 import com.slack.service.murron.trace.Trace;
+
+import java.nio.charset.StandardCharsets;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import org.junit.jupiter.api.Test;
@@ -32,5 +35,40 @@ public class SpanFormatterTest {
     assertThat(actualValue.getKey()).isEqualTo(expectedValue.getKey());
     assertThat(actualValue.getVStr())
         .isEqualTo(expectedValue.getVStr().substring(0, SpanFormatter.MAX_TERM_LENGTH));
+
+      // schema type: TEXT, key: error, value: test error message (less than 32766)
+      expectedValue.setKey("error");
+      expectedValue.setFieldType(Schema.SchemaFieldType.TEXT);
+      expectedValue.setVStr("test error message");
+      actualValue = SpanFormatter.makeTraceKV("error", "test error message", Schema.SchemaFieldType.TEXT);
+      assertThat(actualValue.getKey()).isEqualTo(expectedValue.getKey());
+      assertThat(actualValue.getVStr()).isEqualTo(expectedValue.getVStr());
+
+      // schema type: TEXT, key: error, value: 1234...9999 (greater than 32766)
+      errorMsg = IntStream.range(1, 10000).boxed().map(String::valueOf).collect(Collectors.joining(""));
+      expectedValue.setKey("error");
+      expectedValue.setFieldType(Schema.SchemaFieldType.TEXT);
+      expectedValue.setVStr(errorMsg);
+      actualValue = SpanFormatter.makeTraceKV("error", errorMsg, Schema.SchemaFieldType.TEXT);
+      assertThat(actualValue.getKey()).isEqualTo(expectedValue.getKey());
+      assertThat(actualValue.getVStr())
+              .isEqualTo(expectedValue.getVStr().substring(0, SpanFormatter.MAX_TERM_LENGTH));
+
+      // schema type: BINARY, key: error, value: test error message (less than 32766)
+      expectedValue.setKey("error");
+      expectedValue.setFieldType(Schema.SchemaFieldType.BINARY);
+      expectedValue.setVBinary(ByteString.copyFrom("test error message", StandardCharsets.UTF_8));
+      actualValue = SpanFormatter.makeTraceKV("error", "test error message", Schema.SchemaFieldType.BINARY);
+      assertThat(actualValue.getKey()).isEqualTo(expectedValue.getKey());
+      assertThat(actualValue.getVBinary()).isEqualTo(expectedValue.getVBinary());
+
+      // schema type: BINARY, key: error, value: 1234...9999 (greater than 32766)
+      errorMsg = IntStream.range(1, 10000).boxed().map(String::valueOf).collect(Collectors.joining(""));
+      expectedValue.setKey("error");
+      expectedValue.setFieldType(Schema.SchemaFieldType.BINARY);
+      expectedValue.setVBinary(ByteString.copyFrom(errorMsg, StandardCharsets.UTF_8));
+      actualValue = SpanFormatter.makeTraceKV("error", errorMsg, Schema.SchemaFieldType.BINARY);
+      assertThat(actualValue.getKey()).isEqualTo(expectedValue.getKey());
+      assertThat(actualValue.getVBinary()).isEqualTo(expectedValue.getVBinary().substring(0, SpanFormatter.MAX_TERM_LENGTH));
   }
 }
