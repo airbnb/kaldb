4b6fb8e7a5768337110e6a079407b461c6ecab49
suman_karumuri <suman.karumuri@airbnb.com>
2025-03-28 00:09:07 -0700

    Add parquet file export and scan support.

--- astra/src/main/java/com/slack/astra/chunk/DuckdbSample.java
+++ astra/src/main/java/com/slack/astra/chunk/DuckdbSample.java
@@ -12,12 +12,14 @@ public class DuckdbSample {
 
   public static void main() throws SQLException {
     Properties connectionProperties = new Properties();
-    connectionProperties.setProperty("temp_directory", "/Users/suman_karumuri/duckdb_test");
+    // connectionProperties.setProperty("temp_directory", "/Users/suman_karumuri/temp/duckdb_test");
     Connection conn =
-        DriverManager.getConnection("jdbc:duckdb:/tmp/my_database", connectionProperties);
+        DriverManager.getConnection(
+            "jdbc:duckdb:/tmp/duckdb_test/duckdb_test_db", connectionProperties);
 
     Statement stmt = conn.createStatement();
-    stmt.execute("CREATE TABLE items (item VARCHAR, value DECIMAL(10, 2), count INTEGER)");
+    stmt.execute(
+        "CREATE OR REPLACE TABLE items (item VARCHAR, value DECIMAL(10, 2), count INTEGER)");
     // insert two items into the table
     stmt.execute("INSERT INTO items VALUES ('jeans', 20.0, 1), ('hammer', 42.2, 2)");
 
@@ -27,6 +29,24 @@ public class DuckdbSample {
         System.out.println(rs.getInt(3));
       }
     }
+
+    final String parquetFilePath = "/tmp/duckdb_test/items.parquet";
+    // Make parquet file.
+    stmt.execute(String.format("COPY items TO '%s' (FORMAT parquet)", parquetFilePath));
+    System.out.println("Exported to parquet file successfully " + parquetFilePath);
+
+    System.out.println("Start parquet file read");
+    final String parquet_scan_query =
+        String.format("SELECT * FROM parquet_scan('%s')", parquetFilePath);
+    try (ResultSet rs = stmt.executeQuery(parquet_scan_query)) {
+      while (rs.next()) {
+        System.out.println(rs.getString(1));
+        System.out.println(rs.getInt(3));
+      }
+    }
+    System.out.println("Finished parquet file read");
+
     stmt.close();
+    conn.close();
   }
 }
