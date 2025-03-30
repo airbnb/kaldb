d64c412f7db74a2ed1d2bc4524664d5b2e6efed2
suman_karumuri <suman.karumuri@airbnb.com>
2025-03-30 04:48:34 -0700

    Update sample with map for trace table.

--- astra/src/main/java/com/slack/astra/chunk/DuckdbSample.java
+++ astra/src/main/java/com/slack/astra/chunk/DuckdbSample.java
@@ -13,7 +13,8 @@ public class DuckdbSample {
   public static void main() throws SQLException {
     Connection conn = getConnection();
     Statement stmt = conn.createStatement();
-    insertData(stmt);
+    // insertData(stmt);
+    insertTraceData(stmt);
     final String parquetFilePath = "/tmp/duckdb_test/items.parquet";
     // Make parquet file.
     exportParquet(stmt, parquetFilePath);
@@ -31,13 +32,13 @@ public class DuckdbSample {
     try (ResultSet rs = stmt.executeQuery(parquet_scan_query)) {
       while (rs.next()) {
         System.out.println(rs.getString(1));
-        System.out.println(rs.getInt(3));
+        System.out.println(rs.getString(3));
       }
     }
     System.out.println("Finished parquet file read");
   }
 
-  private static void insertData(Statement stmt) throws SQLException {
+  public static void insertData(Statement stmt) throws SQLException {
     stmt.execute(
         "CREATE OR REPLACE TABLE items (item VARCHAR, value DECIMAL(10, 2), count INTEGER)");
     // insert two items into the table
@@ -47,6 +48,35 @@ public class DuckdbSample {
       while (rs.next()) {
         System.out.println(rs.getString(1));
         System.out.println(rs.getInt(3));
+      }
+    }
+  }
+
+  private static void insertTraceData(Statement stmt) throws SQLException {
+    // TODO: Use INTERVAL for duration?
+    // TODO: UUID for id, trace_id and parent_id. May be not since bytes can be string.
+    // TODO: How is map stored? Need a test. chatgpt says it is encoded. Needs verification. Can't
+    // find in docs.
+    stmt.execute(
+        "CREATE OR REPLACE TABLE items (id STRING, parent_id STRING, trace_id STRING, name STRING, "
+            + "timestamp TIMESTAMP, duration FLOAT, stringMap MAP(STRING, STRING), "
+            + "numericMap MAP(STRING, FLOAT), integerMap MAP(STRING, STRING))");
+
+    String sql1 =
+        """
+            INSERT INTO items VALUES (
+                '1', NULL, 'trace-123', 'root-span', TIMESTAMP '2025-03-30 10:00:00', 120.5,
+                MAP(['http.method', 'http.status_code'], ['GET', '200']),
+                MAP(['db.duration', 'cpu.usage'], [12.3, 55.5]),
+                MAP(['retry_count', 'attempt'], ['1', '2'])
+            );
+        """;
+    stmt.execute(sql1);
+
+    try (ResultSet rs = stmt.executeQuery("SELECT * FROM items")) {
+      while (rs.next()) {
+        System.out.println(rs.getString(1));
+        System.out.println(rs.getString(3));
       }
     }
   }
