b93da132a09189d192364477698b74882c9f5bbf
suman_karumuri <suman.karumuri@airbnb.com>
2025-03-30 00:59:55 -0700

    tiny

--- astra/src/main/java/com/slack/astra/logstore/DuckdbIndexStoreImpl.java
+++ astra/src/main/java/com/slack/astra/logstore/DuckdbIndexStoreImpl.java
@@ -1,39 +1,19 @@
 package com.slack.astra.logstore;
 
-import com.slack.astra.logstore.schema.SchemaAwareLogDocumentBuilderImpl;
 import com.slack.astra.metadata.schema.LuceneFieldDef;
-import com.slack.astra.proto.config.AstraConfigs;
-import com.slack.astra.util.RuntimeHalterImpl;
 import com.slack.service.murron.trace.Trace;
-import io.micrometer.core.instrument.Counter;
-import io.micrometer.core.instrument.MeterRegistry;
-import org.apache.commons.io.FileUtils;
 import org.apache.lucene.index.IndexCommit;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.search.SearcherManager;
-import org.apache.lucene.search.Sort;
-import org.apache.lucene.search.SortField;
 import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.store.MMapDirectory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.Duration;
-import java.util.NoSuchElementException;
-import java.util.Optional;
 import java.util.Properties;
-import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.ReentrantLock;
 
 /*
  * RocksdbIndexStore stores a log message in a duckdb database.
