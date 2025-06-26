f3b0b5d74d01aec17f61e9c1bb04b8414d106c16
Nora Howard <nora.howard@airbnb.com>
2025-06-26 14:26:25 -0600

    add comment to padding constant

--- astra/src/main/java/com/slack/astra/server/ManagerApiGrpc.java
+++ astra/src/main/java/com/slack/astra/server/ManagerApiGrpc.java
@@ -53,7 +53,9 @@ public class ManagerApiGrpc extends ManagerApiServiceGrpc.ManagerApiServiceImplB
   private final SnapshotMetadataStore snapshotMetadataStore;
 
   public static final long MAX_TIME = Long.MAX_VALUE;
-  // TODO: add to config
+  // When partition assignments are updated, we set the start time to this amount of time into the future
+  // to ensure that all services have time to pick up the assignment before it becomes live.
+  // TODO: add to config or a system property so that it can be adjusted if needed.
   public static final long PARTITION_START_TIME_PADDING_MS = Duration.ofMinutes(15).toMillis();
 
   private final ReplicaRestoreService replicaRestoreService;
