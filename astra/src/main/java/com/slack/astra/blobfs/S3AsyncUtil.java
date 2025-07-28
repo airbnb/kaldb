8d3d69b7ef3e416e90bf38c2d75a4045b3e0aca1
Ekagra Gupta <egupta3@asu.edu>
2025-07-27 21:23:28 -0700

    Added maxoffsetdelay config. Fixed local docker S3 mock.

--- astra/src/main/java/com/slack/astra/blobfs/S3AsyncUtil.java
+++ astra/src/main/java/com/slack/astra/blobfs/S3AsyncUtil.java
@@ -82,6 +82,9 @@ public class S3AsyncUtil {
           throw new RuntimeException(e);
         }
       }
+      if (config.getS3ForcePathStyle()) {
+        s3AsyncClient.forcePathStyle(true);
+      }
       return s3AsyncClient.build();
     } catch (S3Exception e) {
       throw new RuntimeException("Could not initialize S3blobFs", e);
