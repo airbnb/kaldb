650a0f463f60fc33c03f2538b2e47e32d6ae93bb
Aubrey Tatarowicz <aubrey.tatarowicz@gmail.com>
2025-11-04 16:56:24 -0600

    special case handling to look up traceId in both base64 and hex

--- astra/src/main/java/com/slack/astra/zipkinApi/ZipkinService.java
+++ astra/src/main/java/com/slack/astra/zipkinApi/ZipkinService.java
@@ -71,7 +71,8 @@ public class ZipkinService {
       @Param("maxSpans") Optional<Integer> maxSpans,
       @Header("X-User-Request") Optional<Boolean> userRequest,
       @Header("X-Data-Freshness-In-Seconds") Optional<Long> dataFreshnessInSeconds,
-      @Header("X-DD-TRACE-ID") Optional<Boolean> ddTraceIdEnabled)
+      @Header("X-DD-TRACE-ID") Optional<Boolean> ddTraceIdEnabled,
+      @Header("X-E2E-hex-base64") Optional<Boolean> searchByHexAndBase64)
       throws IOException {
     String output =
         this.traceFetcher.getByTraceId(
@@ -81,7 +82,8 @@ public class ZipkinService {
             maxSpans,
             userRequest,
             dataFreshnessInSeconds,
-            ddTraceIdEnabled);
+            ddTraceIdEnabled,
+            searchByHexAndBase64);
     return HttpResponse.of(HttpStatus.OK, MediaType.JSON_UTF_8, output);
   }
 }
